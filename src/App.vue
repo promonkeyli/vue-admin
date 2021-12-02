1155f35a8d996cd8a59f8c73742d5f1cd381668e
Young <liyang_email@163.com>
2021-12-02 13:07:34 +0800

    vue3项目初始化脚手架配置

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Home></Home>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Home from './page/home/home.vue'; // 此处vue3要跟完整的文件名
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 });
 </script>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/page/home/home.vue
@@ -0,0 +1,11 @@
+<template>
+   我是vue3定义的home组件页面
+</template>
+
+<script lang="tsx">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "home"
+})
+</script>
