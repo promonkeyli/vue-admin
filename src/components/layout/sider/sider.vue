5b799fbf88c144849593df3c708b1cfef1847198
Young <liyang_email@163.com>
2021-12-03 14:06:41 +0800

    vue3快捷模板搭建/界面主体颜色定义

---
+++ src/components/layout/sider/sider.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-container">
+    我是side组件
+  </div>
+</template>
+
+<script lang="tsx">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: '',
+  components: {
+
+  }
+});
+</script>
+
+<style scoped lang="less">
+
+
+</style>
